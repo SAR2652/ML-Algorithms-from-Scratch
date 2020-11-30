9c80a14abe9c4df10d7513b7189f1164e85d7471
SAR2652 <sar265278@gmail.com>
2020-11-30 21:22:44 +0530

    Improved ULR with additional plots

--- ULR.py
+++ ULR.py
@@ -1,9 +1,10 @@
 import random
+from matplotlib.pyplot import title
 import numpy as np
 import matplotlib.pyplot as plt
 
-X = np.array([x + random.randint(-10, 10) for x in range(100)])
-y = np.array([x + random.randint(-2000, 2000) for x in range(0, 10000, 100)])
+X = 3 * np.random.randn(100, 1)
+y = 7 + 40 * np.random.randn(100, 1)
 print("X:")
 print(X)
 print("y:")
@@ -31,7 +32,17 @@ def gradient_descent(theta0, theta1, learning_rate, X, m):
     temp1 = tune_theta(theta0, theta1, X, learning_rate, m, term = 'weight')
     return temp0, temp1
 
+# Set to default values
+n_iter = 1000
+learning_rate = 0.001
+
+cost_history = []
+theta0_history = []
+theta1_history = []
+
 def train_regression_model(X, y, learning_rate = 0.0001, n_iter = 10000):
+    """This algorithm requires a learning rate of 0.1 or lesser due to the values of the data taken as input. A larger learning rate can be used for different input values"""
+
     # Initialise with temporary values
     theta0 = random.randint(-100, 100)
     theta1 = random.randint(-100, 100)
@@ -41,11 +52,32 @@ def train_regression_model(X, y, learning_rate = 0.0001, n_iter = 10000):
     print("Initial Loss = {}".format(cost))
     for i in range(n_iter):
         theta0, theta1 = gradient_descent(theta0, theta1, learning_rate, X, m)
-        new_cost = cost_function(theta0, theta1, X, y, m)
-        print("Loss at Iteration {} = {}".format(i + 1, new_cost))
+        cost = cost_function(theta0, theta1, X, y, m)
+        cost_history.append(cost)
+        theta0_history.append(theta0)
+        theta1_history.append(theta1)
+        print("Loss at Iteration {} = {}".format(i + 1, cost))
     return theta0, theta1
 
-bias, weight = train_regression_model(X, y)
+bias, weight = train_regression_model(X, y, learning_rate, n_iter)
 print("Bias = {}".format(bias))
 print("Weight = {}".format(weight))
+
+plt.title('Change in Cost Function')
+plt.xlabel('Iterations')
+plt.ylabel('Cost Function')
+plt.plot([i for i in range(1, n_iter + 1)], cost_history)
+plt.show()
+
+plt.xlabel('Iterations')
+plt.ylabel('Bias')
+plt.title('Change in Bias')
+plt.plot([i for i in range(1, n_iter + 1)], theta0_history)
+plt.show()
+
+plt.xlabel('Iterations')
+plt.ylabel('Feature Weight')
+plt.title('Change in Feature Weight')
+plt.plot([i for i in range(1, n_iter + 1)], theta1_history)
+plt.show()
 
